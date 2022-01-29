12f4b92eefead3e5edfd2ce3ba0358b780245b1e
Alexander Gridasov <44508235+alvigr@users.noreply.github.com>
2022-01-29 23:15:01 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Catalog from './pages/Catalog';
+import Cart from './pages/Cart';
 
-function App({ initialState }) {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{initialState.dealers.map(d => `${d},`)}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>DevMarket</h1>
+        <ul>
+          <li>
+            <Link to="/">Catalog</Link>
+          </li>
+          <li>
+            <Link to="cart">Cart</Link>
+          </li>
+        </ul>
       </header>
+      <Routes>
+        <Route path="/" element={<Catalog />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { store } from './store';
 import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { store } from './store';
 import { loadProductsAsync } from './reducers/catalogSlice';
+import App from './App';
+import './index.css';
 
 function start(initialState) {
   store.dispatch(loadProductsAsync(initialState.dealers));
@@ -12,7 +13,9 @@ function start(initialState) {
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <Router>
+          <App />
+        </Router>
       </Provider>
     </React.StrictMode>,
     document.getElementById('root'),

fcfc52392a09171a66894faa98722c987e928627
Alexander <a.gridasov@bookinganalytics.com>
2022-01-29 14:16:33 +0300

    Init app by request

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function start(initialState) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App initialState={initialState} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+window.app = { start }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
